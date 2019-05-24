e3d45557bd7d543f3fd52bdf728447d7cf4aff1d
Zhain HELM <zhelm@c5r4s3.wethinkcode.co.za>
2019-05-24 10:14:06 +0200

    almost

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,40 +6,47 @@
 /*   By: zhelm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 13:44:06 by zhelm             #+#    #+#             */
-/*   Updated: 2019/05/23 14:29:06 by zhelm            ###   ########.fr       */
+/*   Updated: 2019/05/24 10:13:41 by zhelm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bsd/string.h>
+#include <string.h>
 #include <stdio.h>
 
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t n;
-	char *dest = (char *)dst;
-	const char *source = (const char *)src;
+	size_t a;
+	size_t b;
 
-	n = 0;
+	a = 0;
+	b = 0;
 	i = 0;
-	while(dest[i])
-		i++;
-	while(source[n] && i < dstsize -1)
+	a = strlen(dst);
+	b = strlen(src);
+	if(dstsize - 1 < a + b || dstsize == 0)
+		return 0;
+	if(a + b >= dstsize - 1)
 	{
-		dest[i] = source[n];
-		n++;
-		i++;
+		while(a + i < dstsize - 1 && src[i])
+		{
+			dst[a + i] = src[i];
+			i++;
+		}
+		dst[a + i] = '\0';
 	}
-	dest[i] = '\0';
-	return (i);
-}
+	return (a + b);
 
+}
 int main()
 {
-	char c[18] = "hello World";
-	char b[] = "hello          hello";
-	printf("%ld", ft_strlcat(c, b, 18));
-//	printf("%ld", strlcat(c, b, 30));
-	printf("%s", c);
+	char *a= "hello World";
+	char *b = "hellohello";
+	char *c = "Hello World";
+	char *d = "HelloHello";
+	printf("%zu\n", ft_strlcat(a, b, 50));
+	printf("%s\n", a);
+	printf("%zu\n", strlcat(c, d, 50));
+	printf("%s\n", c);
 	return 0;
 }
