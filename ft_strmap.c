11bd68780a3618ea1e520a64f17aff619c317721
zhelm <zhelm@student.wethinkcode.co.za>
2019-05-27 21:37:06 +0200

    Tried something

--- ft_strmap.c
+++ ft_strmap.c
@@ -9,11 +9,42 @@
 /*   Updated: 2019/05/27 15:57:41 by zhelm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-char *ft_strmap(char const *s, char (*f)(char))
+char *ft_putchar(char const *s, char *c)
 {
 	size_t i;
 	char *n;
 
-	while()
+	i = 0;
+	while(s[i])
+	{
+		n[i] = s[i + 1];
+		i++;
+	}
+	return (char *)n;
+}
+char *ft_strmap(char const *s, char (*f)(char))
+{
+	size_t i;
+	char *n;
+	size_t len;
+
+	len = 0;
+	i = 0;
+	while(s[len])
+		len++;
+	n = malloc(sizeof(char) * len + 1);
+	while(s[i])
+	{
+		n[i] = f(s[i]);
+		i++;
+	}
+	return n;
+}
+
+int main()
+{
+	char *c ="Hello World";
+	ft_strmap(c, ft_putchar);
 }
