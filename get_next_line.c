6e6851d0655a808e3c000984a835bf989fbe5420
zhelm <zhelm@student.wethinkcode.co.za>
2019-07-07 12:50:51 +0200

    DONE

--- get_next_line.c
+++ get_next_line.c
@@ -1,104 +1,106 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zhelm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/13 10:15:19 by zhelm             #+#    #+#             */
-/*   Updated: 2019/06/18 14:14:57 by zhelm            ###   ########.fr       */
+/*   Created: 2019/05/28 13:20:10 by zhelm             #+#    #+#             */
+/*   Updated: 2019/06/13 08:48:23 by zhelm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "get_next_line.h"
-#include "libft.h"
 
-size_t ft_strlen(const char *c)
+int     ft_cutter(int rd, char **econtent, char **line)
 {
-	size_t i;
-
-	i = 0;
-	while(c[i])
-		i++;
-	return i;
+    char *tmp;
+    if(*econtent && ft_strchr(*econtent, '\n'))
+    {
+        //join is not proper
+        *line = ft_strcdup(*econtent, '\n');
+        tmp = ft_strdup(ft_strchr(*econtent, '\n') + 1);
+        free(*econtent);
+        *econtent = ft_strdup(tmp);
+        free(tmp);
+    }
+   // printf("between");
+    else if (rd == 0)
+    {
+     //   printf("Hello I get here\n");
+        *line = ft_strdup(*econtent);
+        free(*econtent);
+    }
+    return 1;
 }
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t i;
-	size_t a;
-	size_t b;
 
-	a = 0;
-	i = 0;
-	b = ft_strlen(src);
-	while(dst[a] && a < dstsize)
-		a++;
-	if(a + 1 + i < dstsize)
+int ft_reader(int fd, char **econtent, char **line)
+{
+    char *buff;
+    char *tmp;
+    ssize_t rd;
+
+    rd = 1;
+    while(rd > 0)
+    {
+        buff = ft_memalloc(BUFF_SIZE);
+        rd = read(fd, buff, BUFF_SIZE);
+        if (*econtent == NULL && rd != 0)
+            *econtent = ft_strdup(buff);
+        else if (rd != 0)
+        {
+          
+            tmp = ft_strjoin(*econtent, buff);
+            free(*econtent);
+            *econtent = ft_strdup(tmp);
+            free(tmp);
+        }
+        ft_bzero(buff, BUFF_SIZE);
+        free(buff);
+    if((ft_strlen(*econtent) != 0 && ft_strchr(*econtent, '\n')) || (ft_strlen(*econtent) != 0 && rd == 0))
+        return(ft_cutter(rd, &*econtent, line));
+    return 0;
+    }
+    return -1;
+}
+
+int get_next_line(int fd, char **line)
+{
+    static t_list *head;
+    t_list *ptr;
+    char *econtent;
+
+    if (!line || fd < 0 || read(fd, NULL, 0) == -1)
+        return (-1);
+    econtent = NULL;
+	if(head == NULL)
+		head = ft_lstnew(econtent, (BUFF_SIZE + 1));
+    ptr = head;
+	while(ptr->next != NULL && ptr->content_size != fd)
+		ptr = ptr->next;
+	if (ptr->content_size == fd && ptr->content != NULL)
+		econtent = ptr->content;
+	else
 	{
-		while(a + 1 + i < dstsize && src[i])
-		{
-			dst[a + i] = src[i];
-			i++;
-		}
-		dst[a + i] = '\0';
+		ft_lstadd(&head, ft_lstnew(NULL, (BUFF_SIZE + 1)));
+		ptr->content_size = fd;
 	}
-	return (a + b);
+    return (ft_reader(fd, (char **)&ptr->content, line));
 }
 
-int get_next_line(const int fd, char **line)
+int     main()
 {
-	static size_t l;
-	size_t d;
-	size_t i;
-	static char *b1;
-	static size_t a;
-
-	i = 0;
-	d = 0;
-	line = (char**)malloc(sizeof(char *));
-	*line = (char *)malloc(sizeof(char) * (BUFF_SIZE + 2));
-	b1 = (char *)malloc(sizeof(char) * (BUFF_SIZE + 2));
-	read(fd, line[0], BUFF_SIZE);
-	while(i < BUFF_SIZE)
-	{
-		if(a == l)
-		{
-			printf("%c", line[0][i]);
-			if(line[0][i] == '\n' || line[0][i] == EOF)
-			{
-				while(i < BUFF_SIZE)
-				{
-					*b1 = line[0][i] ;
-					i++;
-					b1++;
-				}
-				l++;
-				a++;
-				return 0;
-			}
-		}
-		else if(line[0][i] == '\n' && a != l)
-			a++;
-			//problem lies with line[0] it includes the new line and lets say H so when it is \nH it sees '\n' and discards H. Need to find a way to detecct if the \n is inside the bufffer or at the end, try and get strlen of string an d then if '\n' is found before the end of the string then start the strsub at that point and len of that would be strlen - start;
-		i++;
-	}
-	free(line[0]);
-	get_next_line(fd, line);
-	return 1;
+    int fd;
+    char *line;
+    int ret;
+    ret = 1;
+    fd = open("bible.txt", O_RDONLY);
+    while ((ret = get_next_line(fd, &line)) > 0)
+    {
+        printf("ret = %d\n", ret);
+        printf("%s\n", line);
+       free(line);
+    }
+    return 0;
+    sleep (30);
 }
-int main()
-{
-	size_t i;
-
-	i = 0;
-	int fd;
-	char **a;
-	a = NULL;
-	fd = open("text.txt", O_RDONLY);
-	//	read(fd, c, BUFF_SIZE);
-	//	c[18] = 127;
-	//	printf("%s", c);
-		get_next_line(fd, a);
-		get_next_line(fd, a);
-		get_next_line(fd, a);
-//		get_next_line(fd, a);
-	}
