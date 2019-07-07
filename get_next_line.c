148384c3995f5f2a22063ee9d578743d704a4c60
zhelm <zhelm@student.wethinkcode.co.za>
2019-07-07 15:34:51 +0200

    OK

--- get_next_line.c
+++ get_next_line.c
@@ -12,23 +12,27 @@
 
 #include "get_next_line.h"
 
-int     ft_cutter(int rd, char **econtent, char **line)
+int ft_checker(char **econtent, char **line)
 {
     char *tmp;
+
+    *line = ft_strcdup(*econtent, '\n');
+    tmp = ft_strdup(ft_strchr(*econtent, '\n') + 1);
+    free(*econtent);
+    *econtent = ft_strdup(tmp);
+    free(tmp);
+    return 1;
+}
+
+int     ft_cutter(int rd, char **econtent, char **line, char **buff)
+{
+  //  free(*buff);
     if(*econtent && ft_strchr(*econtent, '\n'))
     {
-        //join is not proper
-        *line = ft_strcdup(*econtent, '\n');
-    
-        tmp = ft_strdup(ft_strchr(*econtent, '\n') + 1);
-        free(*econtent);
-        *econtent = ft_strdup(tmp);
-        free(tmp);
+     ft_checker(econtent, line);
     }
-   // printf("between");
     else if (rd == 0)
     {
-     //   printf("Hello I get here\n");
         *line = ft_strdup(*econtent);
         free(*econtent);
     }
@@ -42,24 +46,22 @@ int ft_reader(int fd, char **econtent, char **line)
     ssize_t rd;
 
     rd = 1;
-    while(rd > 0)
+    buff = ft_memalloc(BUFF_SIZE);
+    while(rd > 0)//I think I need to put check in gnl
     {
-        buff = ft_memalloc(BUFF_SIZE);
+        ft_bzero(buff, BUFF_SIZE);
         rd = read(fd, buff, BUFF_SIZE);
         if (*econtent == NULL && rd != 0)
             *econtent = ft_strdup(buff);
         else if (rd != 0)
         {
-          
             tmp = ft_strjoin(*econtent, buff);
             free(*econtent);
             *econtent = ft_strdup(tmp);
             free(tmp);
         }
-        ft_bzero(buff, BUFF_SIZE);
-        free(buff);
     if((ft_strlen(*econtent) != 0 && ft_strchr(*econtent, '\n')) || (ft_strlen(*econtent) != 0 && rd == 0))
-        return(ft_cutter(rd, &*econtent, line));
+        return(ft_cutter(rd, &*econtent, line, &buff));
     }
     return 0;
 }
@@ -74,35 +76,45 @@ int get_next_line(int fd, char **line)
         return (-1);
     econtent = NULL;
 	if(head == NULL)
+    {
 		head = ft_lstnew(econtent, (BUFF_SIZE + 1));
+		ft_lstadd(&head, ft_lstnew(NULL, (BUFF_SIZE + 1)));
+    }
     ptr = head;
 	while(ptr->next != NULL && ptr->content_size != fd)
 		ptr = ptr->next;
 	if (ptr->content_size == fd && ptr->content != NULL)
-		econtent = ptr->content;
+		econtent = ptr->content;        
 	else
-	{
-		ft_lstadd(&head, ft_lstnew(NULL, (BUFF_SIZE + 1)));
 		ptr->content_size = fd;
-	}
-   
+    if(ft_strlen(econtent) != 0 && ft_strchr(econtent, '\n') != NULL)
+        return ft_checker((char **)&ptr->content, line);
     return (ft_reader(fd, (char **)&ptr->content, line));
 }
-
+# include <time.h>
 int     main()
 {
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
     int fd;
     char *line;
     int ret;
     ret = 1;
- 
-    fd = open("test2.txt", O_RDONLY);
+    int i = 0;
+
+    fd = open("bible.txt", O_RDONLY);
     while (get_next_line(fd, &line) > 0)
     {
-        printf("ret = %d\n", ret);
+        i++;
+      //  printf("ret = %d\n", ret);
         printf("%s\n", line);
         free(line);
     }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("time = %f", cpu_time_used);
+    
     return 0;
     //sleep (30);
 }
